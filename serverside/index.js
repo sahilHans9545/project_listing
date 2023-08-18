bbe067bf9c613a1b9a90b0fc79a78617a84b98e7
Sahil <hanss9545@gmail.com>
2023-08-18 12:35:39 +0530

    loginRoute File imported

--- serverside/index.js
+++ serverside/index.js
@@ -143,55 +143,55 @@ app.post("/signup", bodyParser.json(), async (req, res) => {
 const loginRoute = require("./routes/loginRoute");
 
 // Use route files
-// app.use("/api", loginRoute);
+app.use(loginRoute);
 
 // **** login ******
 
-app.post("/login", bodyParser.json(), async (req, res) => {
-  console.log(process.env.SECRET_KEY);
-  const { email, password } = req.body;
+// app.post("/login", bodyParser.json(), async (req, res) => {
+//   console.log(process.env.SECRET_KEY);
+//   const { email, password } = req.body;
 
-  // //  email format validation
-  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(email)) {
-    return res.status(400).json({ message: "Invalid email format." });
-  }
+//   // //  email format validation
+//   var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//   if (!emailRegex.test(email)) {
+//     return res.status(400).json({ message: "Invalid email format." });
+//   }
 
-  // password minimum length validation
-  if (password.length < 5) {
-    return res
-      .status(400)
-      .json({ message: "Password must be at least 5 characters long." });
-  }
+//   // password minimum length validation
+//   if (password.length < 5) {
+//     return res
+//       .status(400)
+//       .json({ message: "Password must be at least 5 characters long." });
+//   }
 
-  try {
-    const existingUser = await user.findOne({ email: email });
-    if (existingUser) {
-      const matchPassword = await bcrypt.compare(
-        password,
-        existingUser.password
-      );
-      if (!matchPassword) {
-        return res.status(400).json({ message: "Invalid Credentials" });
-      }
+//   try {
+//     const existingUser = await user.findOne({ email: email });
+//     if (existingUser) {
+//       const matchPassword = await bcrypt.compare(
+//         password,
+//         existingUser.password
+//       );
+//       if (!matchPassword) {
+//         return res.status(400).json({ message: "Invalid Credentials" });
+//       }
 
-      const token = jwt.sign(
-        {
-          email: existingUser.email,
-          id: existingUser._id,
-          mobile_num: existingUser.mobile,
-        },
-        process.env.SECRET_KEY
-      );
-      https: return res.status(201).json({ user: existingUser, token: token });
-    } else {
-      return res.status(404).json({ message: "User not found" });
-    }
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: "Something went wrong !" });
-  }
-});
+//       const token = jwt.sign(
+//         {
+//           email: existingUser.email,
+//           id: existingUser._id,
+//           mobile_num: existingUser.mobile,
+//         },
+//         process.env.SECRET_KEY
+//       );
+//       https: return res.status(201).json({ user: existingUser, token: token });
+//     } else {
+//       return res.status(404).json({ message: "User not found" });
+//     }
+//   } catch (error) {
+//     console.log(error);
+//     return res.status(500).json({ message: "Something went wrong !" });
+//   }
+// });
 
 // Add product applyFilter
 app.post("/addproduct", auth, async (req, res) => {
